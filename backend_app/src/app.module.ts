aa037de8570044234b29718e8a9081704324b3ff
Ferwin Arias <ferwin.arias@gmail.com>
2023-11-24 00:11:49 -0500

    Creating the initial settings of the project in NestJS, cofiguring TypeORM with MySQL and Docker

---
+++ backend_app/src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get('DB_HOST'),
+      port: +configService.get('DB_PORT'),
+      username: configService.get('DB_USER'),
+      password: configService.get('DB_PASS'),
+      database: configService.get('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  }),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      global: true,
+      signOptions: {
+        expiresIn: '1d',
+      }
+    }),
+    inject: [ConfigService],
+  }),],
+  controllers: [AppController],
+  providers: [AppService, JwtService],
+})
+export class AppModule {}
