80f04fb55394b4c65d322cfbf8f6e056b70695b3
Map4che <ljuan648l@gmail.com>
2023-11-24 22:18:21 -0500

    Login y navbar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,26 +1,27 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./Home/Home"
-import SignUp from "./SignUp/SignUp"
-import Login from "./Login/Login"
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import SignUp from "./SignUp/SignUp";
+import Login from "./Login/Login";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div id="App" className="AppContainer">
         <BrowserRouter>
-        <div className="main_app">
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/signUp' element={<SignUp/>}/>
-            <Route path='/login' element={<Login/>}/>
-          </Routes>
+          <div className="main_app">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signUp" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
           </div>
         </BrowserRouter>
-     </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- front-end/src/Login/Login.jsx
+++ front-end/src/Login/Login.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  IconButton,
+  InputAdornment,
+  Box,
+  TextField,
+  Button,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default Login
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (MouseEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="container-login">
+      <div className="login-box">
+        <Box
+          sx={{ display: "flex", alignItems: "flex-end", fontSize: "2.4rem" }}
+        >
+          <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+          <TextField
+            sx={{ width: "24rem", fontSize: "2.4rem" }}
+            id="input-with-sx"
+            label="Username"
+            variant="standard"
+          />
+        </Box>
+        <br />
+        <br />
+        <FormControl
+          sx={{ m: 1, width: "24rem", fontSize: "24px" }}
+          variant="outlined"
+        >
+          <InputLabel
+            sx={{ fontSize: "1.8rem", marginBottom: "2rem" }}
+            htmlFor="outlined-adornment-password"
+          >
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <div className="container-login__button">
+          <Button className="login-button" variant="contained">
+            Login
+          </Button>
+        </div>
+
+        <p className="link-registerp">
+          No tienes una cuenta? <Link to="/signUp">Registrate!</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
