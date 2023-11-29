b369bcceb135e447f70a77786121cd0b1237f869
Juan Fernandez <jmf798@gmail.com>
2023-11-28 21:41:23 -0300

    docs: mentor-category swagger docs

--- backend_app/src/mentor/class/Categories/createCategories.dto.ts
+++ backend_app/src/mentor/class/Categories/createCategories.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 
 export class createCategories {
+  @ApiProperty({
+    description: 'Category name',
+    nullable: false,
+    minLength: 4,
+    example: 'Java',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(4)
