2ebba6a2c1844a98ec3c6cb0e87dc952fbc82ed2
horacio <141278366+horaciodevpro@users.noreply.github.com>
2023-12-06 17:26:57 +0100

    feat Home: add main section

---
+++ front-end/src/components/HomeMainSection.jsx
@@ -0,0 +1,35 @@
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+export default function HomeMainSection({ foto, title, description }) {
+  return (
+    <Paper component='section' sx={{
+      marginTop: '50px',
+      position: 'relative',
+      backgroundColor: 'grey.800',
+      color: '#fff',
+      mb: 4,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url(${foto})`,
+    }}
+    >
+      <Box sx={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, backgroundColor: 'rgba(0,0,0,.4)', }} />
+      <Grid container>
+        <Grid item md={6}>
+          <Box sx={{ position: 'relative', p: { xs: 3, md: 6 }, pr: { md: 0 } }}>
+            <Typography component="h1" variant="h2" color="inherit" gutterBottom sx={{ color: "#007FFF", fontWeight: 'bold', fontSize: { xs: '2.8rem', sm: '4rem' } }}>
+              {title}
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              {description}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  )
+}
