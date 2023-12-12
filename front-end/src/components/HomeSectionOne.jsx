c118121f2bad40fe47b681fa27ec439a446864f4
horacio <141278366+horaciodevpro@users.noreply.github.com>
2023-12-12 04:11:52 +0100

    fix: Home, for error (varia) and key

--- front-end/src/components/HomeSectionOne.jsx
+++ front-end/src/components/HomeSectionOne.jsx
@@ -9,13 +9,13 @@ export default function HomeSectionOne() {
   return (
     <Container component='section' sx={{ marginTop: '5rem' }}>
       <Box sx={{ mb: 3 }}>
-        <Typography component='h4' variant="h4" varia sx={{ color: '#F9F9F9' }}>
+        <Typography component='h4' variant="h4" sx={{ color: '#F9F9F9' }}>
           Lo que te ofrece nuestra plataforma
         </Typography>
       </Box>
       <Grid container spacing={6} style={{ marginBottom: "5rem" }}>
         {dataHomeJson.map(data =>
-          <Grid item key={data} xs={12} sm={6} md={4} lg={4}>
+          <Grid item key={data.title} xs={12} sm={6} md={4} lg={4}>
             <CardHomeText
               image={data.picture}
               title={data.title}
