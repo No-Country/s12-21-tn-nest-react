c4536f882cb5befb0967a0b5b8497b74394bc763
horacio <141278366+horaciodevpro@users.noreply.github.com>
2023-12-18 17:44:05 +0100

    style: Login, modified

--- front-end/src/Login/Login.jsx
+++ front-end/src/Login/Login.jsx
@@ -11,11 +11,30 @@ import {
   InputAdornment,
   Box,
   TextField,
+  Container,
+  Button,
 } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import "./Login.css";
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#25D366',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+    background: {
+      paper: '#111b21',
+      default: '#0B141A',
+    },
+  },
+})
 
 const Login = () => {
   const navigate = useNavigate();
@@ -47,98 +66,109 @@ const Login = () => {
 
   return (
     <>
-      {loginErrors &&
-        loginErrors.length > 0 &&
-        loginErrors.map((error, i) => (
-          <div
-            style={{
-              background: "red",
-              color: "white",
-              textAlign: "center",
-            }}
-            key={i}
-          >
-            {error}
-          </div>
-        ))}
-      <form className="container-login" onSubmit={login}>
-        <div className="login-box">
-          <Box
-            className="container-login__box"
-            sx={{ textAlign: "center", margin: "1rem auto" }}
-          >
-            <AccountCircle sx={{ color: "white", mr: 1, my: 0.5 }} />
-            <div
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <TextField
-                id="input-with-sx"
-                label="Email"
-                variant="standard"
-                className="input-username"
-                type="email"
-                required
-                {...register("email", { required: true })}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-                InputProps={{
-                  style: { color: "white", fontSize: "0.5em" },
-                }}
-              />
-              {errors.email && (
-                <p style={{ color: "#fa4444", fontSize: "1.8rem" }}>
-                  Username is required
-                </p>
-              )}
-            </div>
-          </Box>
-          <FormControl sx={{ m: 0 }} variant="outlined">
-            <InputLabel
-              sx={{ color: "white" }}
-              htmlFor="outlined-adornment-password"
-            >
-              Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? "text" : "password"}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    sx={{ color: "white" }}
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
+      <ThemeProvider theme={customTheme}>
+        <Container component="section" sx={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "70vh" }}>
+          <Container maxWidth="xs" sx={{ border: { sm: "2px solid #25D366", xs: "none" }, borderRadius: "8px", py: 1, height: "100%", width: "100%" }}>
+
+            {loginErrors &&
+              loginErrors.length > 0 &&
+              loginErrors.map((error, i) => (
+                <div
+                  style={{
+                    background: "red",
+                    color: "white",
+                    textAlign: "center",
+                  }}
+                  key={i}
+                >
+                  {error}
+                </div>
+              ))}
+            <form className="container-login" onSubmit={login}>
+              <div className="login-box">
+                <Box
+                  className="container-login__box"
+                  sx={{ /* textAlign: "center", margin: "1rem auto" */width:"100%" }}
+                >
+                  {/* <AccountCircle sx={{ color: "white", mr: 1, my: 0.5 }} /> */}
+                  <div
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
                   >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
-              inputProps={{
-                style: { color: "white" },
-              }}
-              {...register("password", { required: true })}
-            />
-            {errors.password && (
-              <p style={{ color: "#fa4444", fontSize: "1.8rem" }}>
-                Password is required
-              </p>
-            )}
-          </FormControl>
-          <div>
-            <button className="btn" type="submit">
-              <i className="animation"></i>Ingresa
-              <i className="animation"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+                    <TextField sx={{width: "100%", mt: 2 }}
+                      id="input-with-sx"
+                      label="Email"
+                      variant="standard"
+                      className="input-username"
+                      type="email"
+                      required
+                      {...register("email", { required: true })}
+                      InputLabelProps={{
+                        style: { /* color: "white" */ },
+                      }}
+                      InputProps={{
+                        style: { /* color: "white", fontSize: "0.5em" */ },
+                      }}
+                    />
+                    {errors.email && (
+                      <p style={{ color: "#fa4444"/* , fontSize: "1.8rem" */ }}>
+                        Username is required
+                      </p>
+                    )}
+                  </div>
+                </Box>
+                <FormControl sx={{ /* m: 0 */width: "100%", mt: 2 }} variant="outlined">
+                  <InputLabel
+                    sx={{ /* color: "white" */ }}
+                    htmlFor="outlined-adornment-password"
+                  >
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          sx={{ color: "white" }}
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                    inputProps={{
+                      style: { color: "white" },
+                    }}
+                    {...register("password", { required: true })}
+                  />
+                  {errors.password && (
+                    <p style={{ color: "#fa4444", fontSize: "1.8rem" }}>
+                      Password is required
+                    </p>
+                  )}
+                </FormControl>
+                <div>
+                  {/* <button className="btn" type="submit">
+                    <i className="animation"></i>Ingresa
+                    <i className="animation"></i>
+                  </button> */}
+                  <Button type="submit" variant="contained" sx={{ color: "#FFF", width: "100%", my: 2 }}>
+                    Ingresa
+                  </Button>
+                </div>
+              </div>
+            </form>
+
+          </Container>
+        </Container>
+      </ThemeProvider>
     </>
   );
 };
