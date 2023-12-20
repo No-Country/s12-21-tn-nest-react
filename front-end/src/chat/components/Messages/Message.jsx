d7c00ceb7bbe8867e0e74b7bc1e2502c0f7f3825
MayderC <44930667+MayderC@users.noreply.github.com>
2023-12-20 15:01:57 -0600

    shows the name in the message

--- front-end/src/chat/components/Messages/Message.jsx
+++ front-end/src/chat/components/Messages/Message.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import "./style.css";
+import { AuthContext } from "./../../../context/AuthContext";
 
 export const Message = ({ msg, position }) => {
+  const { user } = useContext(AuthContext);
+
   return (
     <>
       <div className={`message__wrapper${position ? position : ""}`}>
-        <div className="message__user"></div>
+        <div className="message__user">
+          {user.userId === msg.sender.id ? "Me" : msg?.sender?.firstName}
+        </div>
         <div className="message__text">
           <p>{msg?.message}</p>
         </div>
