7cb96b70679aa4e894ce87fa43f8188e65e0fe85
Jareth Guerrero <yaredguerrero@gmail.com>
2023-12-20 14:47:05 -0600

    Realizando cambios pasarela pago

--- front-end/src/components/PagosDonativos.jsx
+++ front-end/src/components/PagosDonativos.jsx
@@ -1,47 +1,70 @@
+
 import { Modal, Box, Typography, useMediaQuery } from '@mui/material';
 import { urlApi } from '../../config/axios';
 
+
 export const PagosDonativos = ({ open, onClose, name, mentorData, mentorPrice }) => {
     const isMobile = useMediaQuery('(max-width:600px)');
-    
-    const redirectPaymentGateway = async (gateway) => {
+    console.log(mentorPrice)
 
+
+    const redirectPaymentGateway = async (gateway) => {
         try {
             switch (gateway) {
                 case 'mercadoPago':
                     const mercadoPagoData = {
-                        external_reference: "a0963252-9ded-11ee-801e-ace2d37bcdbb",
+                        external_reference: "9def4c21-e187-48f2-bfaa-9ddf42f18670", //Id alumnhire 20/dec
                         value: "15.00",
                         email: mentorData.email,
                         brand_name: name
                     };
-                    const responseMercadoPago = await urlApi.post('mpago', mercadoPagoData);
-                    console.log('Respuesta de Mercado Pago: ', responseMercadoPago.data);
-                    console.log('Datos a enviar con mercadopagoData: ',  mercadoPagoData)
+                    try {
+                        const responseMercadoPago = await urlApi.post('mpago', mercadoPagoData);
+                        console.log('Respuesta de Mercado Pago: ', responseMercadoPago.data);
+                        console.log('Datos a enviar con mercadopagoData: ', mercadoPagoData)
+                        if (responseMercadoPago.data && responseMercadoPago.data.url) {
+                            const mercadoPagoUrl = responseMercadoPago.data.url;
+                            window.open(mercadoPagoUrl, '_blank');
+                        } else {
+                            console.error('No se recibió la URL de Mercado Pago en la respuesta.');
+                        }
+                    } catch (error) {
+                        console.error('Error al realizar la solicitud a Mercado Pago: ', error);
+                    }
                     break;
 
                 case 'payPal':
                     const paypalData = {
-                        reference_id: "7ede8acc-9dec-11ee-801e-ace2d37bcdbb", //id alumn_hire_mentor
+                        reference_id: "9def4c21-e187-48f2-bfaa-9ddf42f18670", //Id alumnhire 20/dec
                         currency_code: 'USD',
                         value: "15.00",
                         brand_name: name
                     };
-                    const responsePayPal = await urlApi.post('paypal', paypalData);
-                    console.log('Respuesta de PayPal: ', responsePayPal.data);
-                    console.log('Datos a enviar con payPalData: ', paypalData)
+                    try {
+                        const responsePayPal = await urlApi.post('paypal', paypalData);
+
+                        if (responsePayPal.data && responsePayPal.data.url) {
+                            const payPalUrl = responsePayPal.data.url;
+                            window.open(payPalUrl, '_blank');
+                        } else {
+                            console.error('No se recibió la URL de PayPal en la respuesta.');
+                        }
+                    } catch (error) {
+                        console.error('Error al realizar la solicitud a PayPal: ', error);
+                    }
                     break;
 
                 case 'stripe':
                     const stripeData = {
-                        reference_id: "7ede8acc-9dec-11ee-801e-ace2d37bcdbb",
+                        reference_id: "9def4c21-e187-48f2-bfaa-9ddf42f18670", //Id alumnhire 20/dec
                         currency_code: "USD",
                         amount: mentorPrice,
                         brand_name: name
                     };
                     try {
                         const responseStripe = await urlApi.post('stripe', stripeData);
-
+                        console.log('Respuesta de Stripe: ', responseStripe.data);
+                        console.log('Datos a enviar con stripeData: ', stripeData)
                         // Se abre la URL de Stripe en una nueva ventana
                         if (responseStripe.data && responseStripe.data.url) {
                             const stripeUrl = responseStripe.data.url;
