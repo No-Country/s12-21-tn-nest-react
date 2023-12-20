491dae1c5c0bf7d0e32d07e536b5c4057dfa76b4
MayderC <44930667+MayderC@users.noreply.github.com>
2023-12-20 15:23:49 -0600

    identify left or right for each message

--- front-end/src/chat/pages/PrivateChat.jsx
+++ front-end/src/chat/pages/PrivateChat.jsx
@@ -7,15 +7,19 @@ export const PrivateChat = () => {
   const { messages, sendMessage, userId } = usePrivateChat();
 
   const leftOrRight = (msg) => {
-    return msg.user.id === userId ? "left" : "right";
+    return msg.sender.id === userId ? " left" : " right";
   };
 
   return (
     <>
       <div className="private__chat__container">
         <div className="priv__messages">
-          {messages.map((msg, i) => (
-            <Message key={i} msg={msg}></Message>
+          {messages.map((msg) => (
+            <Message
+              key={msg.id}
+              position={leftOrRight(msg)}
+              msg={msg}
+            ></Message>
           ))}
         </div>
         <InputMessages emmitMessage={sendMessage} />
